39e507cfded91428f44005530f206f12ff561814
StackOverfloweds <bryangithub32@gmail.com>
2024-09-28 16:28:57 +0700

    update: add conf for send msg and runing txt program

--- src/routing/peopleRankActive.java
+++ src/routing/peopleRankActive.java
@@ -14,8 +14,8 @@ public class peopleRankActive extends ActiveRouter {
     public static final String DUMPING_FACTOR_SETTING = "dumpingFactor";
     public static final String TRESHOLD_SETTING = "treshold";
 
-    /** Prophet router's setting namespace ({@value})*/ 
-	public static final String PeopleRank_NS = "PeopleRank";
+    /** PeopleRank router's setting namespace ({@value})*/ 
+	public static final String PeopleRank_NS = "PeopleRankRouter";
     public static final String MSG_COUNT_PROPERTY = PeopleRank_NS + "." +
 		"copies";
 
@@ -28,6 +28,11 @@ public class peopleRankActive extends ActiveRouter {
     protected Map <DTNHost, List<Duration>> connHistory;
     protected Map<DTNHost,Double> startTimeStamps;
     protected Set <DTNHost> thisHostSet;
+
+    /**
+     * Create Constructor
+     * @param s
+     */
     public peopleRankActive (Settings s)
     {
         super(s);
@@ -115,10 +120,6 @@ public class peopleRankActive extends ActiveRouter {
         }
             }
         }
-
-        
-
-    
      /**
      * Calculates the PeopleRank for a given host based on the formula:
      * PeR(Ni) = (1 - d) + d * Σ PeR(Nj) / |F(Nj)|
@@ -173,14 +174,93 @@ public class peopleRankActive extends ActiveRouter {
 
      }
 
+     
 
+      /**
+     * Determines whether a message should be sent from this host to another host
+     * based on the PeopleRank routing algorithm.
+     * 
+     * @param m         The message to be sent.
+     * @param thisHost  The current host from which the message originates.
+     * @param otherHost The destination host to which the message should be sent.
+     * @return True if the message should be sent to the other host, false
+     *         otherwise.
+     */
 
+     private boolean sendMessage (Message m, DTNHost otherHost, DTNHost thisHost)
+     {
+        // Check if the destination of the message is the other host
+        if (m.getTo() == otherHost) {
+            return true; // Message should be sent directly to the destination
+        }
+        // Calculate PeopleRank for this host and other host
+        double perThisHost = calculatePer(thisHost);
+        double perOtherHost = calculatePer(otherHost);
 
+        // Initialize F(i) as the set of friends of i
+        Set<DTNHost> Fi = new HashSet<>(connHistory.keySet());
+        Fi.add(thisHost);
 
+        // Check if this host is in contact with the other host or already friend
+        if (connHistory.containsKey(otherHost) || thisHostSet.contains(otherHost)) {
+            // while 1 do
+            while (true) {
+                // while i is in contact with j do
+                for (Map.Entry<DTNHost, List<Duration>> entry : connHistory.entrySet()) {
+                    if (entry.getKey().equals(otherHost)) {
+                        Iterator<DTNHost> iterator = Fi.iterator();
+                        while (iterator.hasNext()) {
+                            DTNHost check = iterator.next();
+                            if (otherHost.equals(check)) { // if j ∈ F(i) then
+                                // System.out.println("Check Fi : " + check + "other host : " + otherHost);
+                                return true;
+                            } else if (!check.equals(otherHost)) { // if j !∈ F(i) then
+                                // System.out.println("false");
+                                return false;
+                            }
+                        }
+                    }
+                    // while ∃ m ∈ buffer(i) do
+                    Buffer messageBuffer = new Buffer(); // Instantiate Buffer with settings
+                    int bufferSize = messageBuffer.getBufferSize(thisHost);
+                    while (bufferSize > 0) {
+                        if (perOtherHost >= perThisHost || otherHost.equals(m.getTo())) {
+                            return true; // Condition met, Forward
+                        }
+                    }
+                }
 
+                // If the destination host is not in contact with the current host, check the
+                // end while
+            }
+        }
+        return false; // Otherwise, do not send the message to other host
+     }
 
-
-
+     @Override
+     public Message messageTransferred(String id, DTNHost from)
+     {
+        // Retrieve the message that was transferred
+    Message m = super.messageTransferred(id, from);
+    
+    // Get the current host and the other hosts in the network
+    DTNHost thisHost = getHost();
+    List<Connection> connections = thisHost.getConnections();
+    
+    // Loop through the connected hosts
+    for (Connection con : connections) {
+        DTNHost otherHost = con.getOtherNode(thisHost);
+        
+        // Call the PeopleRank routing algorithm to determine if the message should be sent
+        if (sendMessage(m, otherHost, thisHost)) {
+            if (true) { // Example condition; replace with your actual logic
+                return m; // Return the message if it's successfully sent
+            }
+        }
+    }
+    
+    return null; // If no message is forwarded, return null
+     }
     @Override
     public  peopleRankActive replicate () {
         peopleRankActive p = new peopleRankActive(this);
